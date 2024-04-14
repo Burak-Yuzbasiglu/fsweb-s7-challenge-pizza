e6b6849aeb16acad7235697698d22ad0462496fc
Burak-Yuzbasiglu <161696597+Burak-Yuzbasiglu@users.noreply.github.com>
2024-04-14 04:11:17 +0300

    Reapply "itteration-1 Home-orderPizza-Success sayfaların oluşturup bağlanması"
    This reverts commit 0dd02c3f9d1d65c1dbd0b82042eb536854fee662.

--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -4,35 +4,35 @@ import './pizza.css'
 
 function Pizza() {
     const [count, setCount] = useState(0)
-    const [title, setTitle] = useState("burak")
-    const [price, setPrice] = useState(17)
-    const [rate, setRate] = useState(4.7)
+    const [title, setTitle] = useState("Position Absolute Acı Pizza")
+    const [price, setPrice] = useState(85.50)
+    const [rate, setRate] = useState(4.9)
     const [rateCount, setRateCount] = useState(200)
-    const [description, setDescription] = useState("Margarita pizza (İtalyanca: Pizza Margherita), domates, mozarella, fesleğen, zeytinyağı ve tuzla yapılan Napoli pizzasıdır.")
+    const [description, setDescription] = useState("Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.")
 
 
     return (
         <>
-            <div class="container text-center" style={{ backgroundColor: "white" }}>
+            <div style={{ backgroundColor: "white" }}>
                 <div class="row">
                     <div class="col" style={{ textAlign: "left" }}>
-                        <h1>{title}</h1>
+                        <h4 className='pizzaTittle '>{title}</h4>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-8" style={{ textAlign: "left" }}>
-                        <h2>Fiyat {price} TL</h2>
+                        <h2 className='priceTittle'>{price}₺</h2>
                     </div>
-                    <div class="col-2" style={{}}>
-                        <h3>{rate}</h3>
+                    <div class="col-2" >
+                        <h6 className='rate'>{rate}</h6>
                     </div>
-                    <div class="col-2" style={{}}>
-                        <h4>{rateCount}</h4>
+                    <div class="col-2" style={{ textAlign: "right" }}>
+                        <h6 className='rateCount' >({rateCount})</h6>
                     </div>
                 </div>
                 <div class="row" style={{ textAlign: "left" }}>
                     <div class="col">
-                        <h5>{description}</h5>
+                        <p className='description'>{description}</p>
                     </div>
                 </div>
             </div>
